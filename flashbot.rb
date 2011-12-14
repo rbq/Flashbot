3bb59ea02d6fe5816f363e371407a037d0fb7231
Simon Szustkowski <mail@simonszu.de>
2011-12-14 12:11:56 +0100

    Ach, scheiss drauf, der Bot kann auch international arbeiten

--- flashbot.rb
+++ flashbot.rb
@@ -31,9 +31,6 @@ $search['suche']['since_id'] = $since_id
 File.open(SEARCH_FILE, 'w') { |f| YAML.dump($search, f) }
 
 suchergebnisse.each do |ergebnis|
-  if (ergebnis.iso_language_code == "de")
-    puts ergebnis.id
     client.update("@#{ergebnis.from_user} FLASH! Aaaah - Savior of the Universe", {:in_reply_to_status_id => ergebnis.id})
-  end
 end
 
